4c656f656d8155919bda48f4ea8493cf0e00266d
dontTochMyNameFuckingBoy <maraffonskiy@gmail.com>
2017-02-03 17:02:14 +0200

    add info to "home.php" file

---
+++ home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Муга крутой сайт скоро затмит всех."/>
+        <meta name="keywords" content="мена крутой сайт"/>
+        <title>Крутой сайт</title>
+    </head>
+    <body>
+        <h1>Home</h1>
+    </body>
+</html>
